fe9f9d348e750d7983752e1c55bd7efa2ba60a73
poenao <940837938@qq.com>
2026-04-13 05:43:38 +0800

    个人主页模板-custom-tabs和sticky模板

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,38 +1,164 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
+  <scroll-page>
+    <view
+      class="index-page"
+      :style="{ backgroundPositionY: -48 + safeAreaInsets.top + 'px' }"
+    >
+      <!-- 页面导航 -->
+      <view
+        :style="{ paddingTop: safeAreaInsets.top + 'px' }"
+        class="page-navbar"
+        >优医</view
+      >
+
+      <!-- 搜索栏 -->
+      <view class="search-bar">
+        <input
+          placeholder-class="input-placeholder"
+          placeholder="搜一搜: 疾病/症状/医生/健康知识"
+          class="input"
+          type="text"
+        />
+        <view class="icon-search">
+          <uni-icons size="22" color="#C3C3C5" type="search" />
+        </view>
+      </view>
+      <!-- 快速入口 -->
+      <view class="quick-entry">
+        <navigator hover-class="none" class="quick-entry-item">
+          <image
+            class="quick-entry-icon"
+            src="/static/images/quick-entry-1.png"
+          />
+          <text class="label">问医生</text>
+          <text class="small">按科室查问医生</text>
+        </navigator>
+        <navigator
+          hover-class="none"
+          class="quick-entry-item"
+          url="/subpkg_consult/quickly/index"
+        >
+          <image
+            class="quick-entry-icon"
+            src="/static/images/quick-entry-2.png"
+          />
+          <text class="label">极速问诊</text>
+          <text class="small">20s医生极速回复</text>
+        </navigator>
+        <navigator hover-class="none" class="quick-entry-item">
+          <image
+            class="quick-entry-icon"
+            src="/static/images/quick-entry-3.png"
+          />
+          <text class="label">开药门诊</text>
+          <text class="small">线上买药更方便</text>
+        </navigator>
+      </view>
+      <!-- 快速查看 -->
+      <view class="quick-view">
+        <navigator
+          hover-class="none"
+          class="quick-view-item"
+          url="/subpkg_medicine/order_list/index"
+        >
+          <image
+            class="quick-view-icon"
+            src="/static/images/quick-view-1.png"
+          />
+          <text class="label">药品订单</text>
+        </navigator>
+        <navigator
+          hover-class="none"
+          class="quick-view-item"
+          url="/subpkg_archive/list/index"
+        >
+          <image
+            class="quick-view-icon"
+            src="/static/images/quick-view-2.png"
+          />
+          <text class="label">健康档案</text>
+        </navigator>
+        <navigator hover-class="none" class="quick-view-item">
+          <image
+            class="quick-view-icon"
+            src="/static/images/quick-view-3.png"
+          />
+          <text class="label">我的处方</text>
+        </navigator>
+        <navigator hover-class="none" class="quick-view-item">
+          <image
+            class="quick-view-icon"
+            src="/static/images/quick-view-4.png"
+          />
+          <text class="label">疾病查询</text>
+        </navigator>
+      </view>
+      <!-- 广告位 -->
+      <view class="banner-placeholder">
+        <swiper
+          class="uni-swiper"
+          indicator-dots
+          indicator-color="#ffffff99"
+          indicator-active-color="#fff"
+          circular
+        >
+          <swiper-item>
+            <navigator hover-class="none" class="navigator" url=" ">
+              <image class="banner-image" src="/static/images/banner-1.png" />
+            </navigator>
+          </swiper-item>
+          <swiper-item>
+            <navigator hover-class="none" class="navigator" url=" ">
+              <image
+                class="banner-image"
+                src="/static/images/banner-1.png"
+                mode="aspectFill"
+              />
+            </navigator>
+          </swiper-item>
+        </swiper>
+      </view>
+      <!-- 标签页列表 -->
+      <scroll-page>
+        <view
+          class="index-page"
+          :style="{ backgroundPositionY: -48 + safeAreaInsets.top + 'px' }"
+        >
+          <!-- 省略前面小节的代码... -->
+
+          <!-- 标签切换 -->
+          <view
+            class="doctor-feeds"
+            :style="{ marginTop: -safeAreaInsets.top + 'px' }"
+          >
+            <custom-sticky :offset-top="safeAreaInsets.top + 'px'">
+              <custom-tabs @click="onFeedTabChange" :list="feedTabs" />
+            </custom-sticky>
+          </view>
+        </view>
+      </scroll-page>
     </view>
-  </view>
+  </scroll-page>
 </template>
+<script setup>
+  import { ref } from 'vue'
 
-<script setup></script>
-
-<style>
-  .content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  // 获取安全区域数据
+  const { safeAreaInsets } = uni.getSystemInfoSync()
+  // 标签页索引值
+  const tabIndex = ref(0)
+  // 标签页数据
+  const feedTabs = ref([
+    { label: '推荐' },
+    { label: '关注' },
+    { label: '减脂' },
+    { label: '饮食' },
+  ])
+  // 切换标签页
+  function onFeedTabChange({ index }) {
+    tabIndex.value = index
   }
-
-  .logo {
-    height: 200rpx;
-    width: 200rpx;
-    margin-top: 200rpx;
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 50rpx;
-  }
-
-  .text-area {
-    display: flex;
-    justify-content: center;
-  }
-
-  .title {
-    font-size: 36rpx;
-    color: #8f8f94;
-  }
+</script>
+<style lang="scss">
+  @import './index.scss';
 </style>
