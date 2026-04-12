d10ac5ba30639d3b3a1a926317b48c95ed1dd6f2
poenao <940837938@qq.com>
2026-04-12 10:34:04 +0800

    my 我的页面：发请求拿到个人信息渲染到个人信息页面

--- apis/user.js
+++ apis/user.js
@@ -1,21 +1,32 @@
-import { request } from '../utils/request'
+import {
+	request
+} from '../utils/request'
 /**
  * 发送验证码
  */
 
 export const verifyCodeApi = (data) => {
-  return request.get('/code', { params: data })
+	return request.get('/code', {
+		params: data
+	})
 }
 
 /**
  * 用户登录接口（短信验证码方式）
  */
 export const loginByMobileApi = (data) => {
-  return request.post('/login', data)
+	return request.post('/login', data)
 }
 /**
  * 用户登录接口（密码方式）
  */
 export const loginByPassword = (data) => {
-  return request.post('/login/password', data)
+	return request.post('/login/password', data)
+}
+
+/**
+ * 获取用户信息
+ */
+export const userInfoApi = () => {
+	return request.get('/patient/myUser')
 }
--- utils/request.js
+++ utils/request.js
@@ -1,71 +1,83 @@
 // 导入模块
 import Request from 'luch-request'
-import { userInfoStore } from '../stores/user'
-
-const store = userInfoStore
+import {
+	userInfoStore
+} from '@/stores/user.js'
 // tabBar页面路径
 const tabBarList = [
-  'pages/index/index',
-  'pages/wiki/index',
-  'pages/notify/index',
-  'pages/my/index',
+	'pages/index/index',
+	'pages/wiki/index',
+	'pages/notify/index',
+	'pages/my/index',
 ]
 // 实例化网络请求
 const request = new Request({
-  baseURL: 'https://consult-api.itheima.net', // 基础URL
-  timeout: 5000, // 请求超时时间
-  custom: {
-    loading: true, // 是否显示加载提示
-  },
+	baseURL: 'https://consult-api.itheima.net', // 基础URL
+	timeout: 5000, // 请求超时时间
+	custom: {
+		loading: true, // 是否显示加载提示
+	},
 })
-
+// 接口白名单
+const whiteList = ['/code', '/login', '/login/password']
 // 请求拦截器
 request.interceptors.request.use(
-  (config) => {
-    // 在请求头中添加Authorization字段，携带token
-    const token = store.token
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`
-    }
-    return config
-  },
-  (error) => {
-    // 处理请求错误
-    return Promise.reject(error)
-  }
+	(config) => {
+		// 显示加载状态提示
+		const store = userInfoStore()
+		// 全局默认的头信息（方便以后扩展）
+		const defaultHeader = {}
+		// 判断是否存在 token 并且不在接口白单当中
+		if (store.token && !whiteList.includes(config.url)) {
+			defaultHeader.Authorization = 'Bearer ' + store.token
+		}
+		// 合并全局头信息和局部头信息（局部优先级高全局）
+		config.header = {
+			...defaultHeader,
+			...config.header,
+		}
+
+		return config
+	},
+	(error) => {
+		// 处理请求错误
+		return Promise.reject(error)
+	}
 )
 // 响应拦截器
 request.interceptors.response.use(
-  (response) => {
-    // 如果响应状态码是401，说明token无效或过期，提示用户重新登录
-    if (response.statusCode === 401) {
-      uni.utils.toast('登录状态已过期，请重新登录!')
-      relogin()
-      return Promise.reject(new Error('Unauthorized'))
-    }
-    return response.data
-  },
-  (error) => {
-    // 处理响应错误
-    return Promise.reject(error)
-  }
+	(response) => {
+		// 如果响应状态码是401，说明token无效或过期，提示用户重新登录
+		if (response.statusCode === 401) {
+			uni.utils.toast('登录状态已过期，请重新登录!')
+			relogin()
+		}
+		return response.data
+	},
+	(error) => {
+		// 处理响应错误
+		return Promise.reject(error)
+	}
 )
 
 // 引导用户重新登录
 const relogin = () => {
-  // 动态读取当前页面的路径
-  const pageStack = getCurrentPages()
-  const currentPage = pageStack[pageStack.length - 1]
-  // 完整的路由包含地址的参数
-  const redirectURL = currentPage.$page.fullPath
-  // 判断当前页面是否在tabBar中然后选择合适的跳转方式
-  const openType = tabBarList.includes(currentPage.route)
-    ? 'switchTab'
-    : 'navigateTo'
-  //把上一页的页面地址和处理过的跳转方式传递给pinia
-  store.redirectURL = redirectURL
-  store.openType = openType
-  // 跳转到登录页面
-  uni.redirectURL('/pages/login/index')
+	const store = userInfoStore()
+	// 动态读取当前页面的路径
+	const pageStack = getCurrentPages()
+	const currentPage = pageStack[pageStack.length - 1]
+	// 完整的路由包含地址的参数
+	const redirectURL = currentPage.$page.fullPath
+	// 判断当前页面是否在tabBar中然后选择合适的跳转方式
+	const openType = tabBarList.includes(currentPage.route) ?
+		'switchTab' :
+		'navigateTo'
+	//把上一页的页面地址和处理过的跳转方式传递给pinia
+	store.redirectURL = redirectURL
+	store.openType = openType
+	// 跳转到登录页面
+	uni.redirectURL('/pages/login/index')
 }
-export { request }
+export {
+	request
+}
