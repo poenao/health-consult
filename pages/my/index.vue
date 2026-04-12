353266cf48af21611b589f8f5cc721ee256eb24c
poenao <940837938@qq.com>
2026-04-12 10:45:47 +0800

    退出登录-清除个人信息和token 然后重置默认页面和登陆方式

--- pages/my/index.vue
+++ pages/my/index.vue
@@ -1,258 +1,250 @@
 <template>
-	<scroll-page background-color="#F6F7F9">
-		<view class="my-page">
-			<!-- 用户资料（头像&昵称） -->
-			<view class="user-profile">
-				<image class="user-avatar" :src="userInfo.avatar"></image>
-				<view class="user-info">
-					<text class="nickname">{{ userInfo.account }}</text>
-					<text class="iconfont icon-edit"></text>
-				</view>
-			</view>
-			<!-- 用户数据 -->
-			<view class="user-data">
-				<navigator hover-class="none" url=" ">
-					<text class="data-number">{{ userInfo.collectionNumber }}</text>
-					<text class="data-label">收藏</text>
-				</navigator>
-				<navigator hover-class="none" url=" ">
-					<text class="data-number">{{ userInfo.likeNumber }}</text>
-					<text class="data-label">关注</text>
-				</navigator>
-				<navigator hover-class="none" url=" ">
-					<text class="data-number">{{ userInfo.score }}</text>
-					<text class="data-label">积分</text>
-				</navigator>
-				<navigator hover-class="none" url=" ">
-					<text class="data-number">{{ userInfo.couponNumber }}</text>
-					<text class="data-label">优惠券</text>
-				</navigator>
-			</view>
-			<!-- 问诊医生 -->
-			<custom-section
-				:custom-style="{ paddingBottom: '20rpx' }"
-				title="问诊中"
-			>
-				<swiper
-					class="uni-swiper"
-					indicator-active-color="#2CB5A5"
-					indicator-color="#EAF8F6"
-					indicator-dots
-				>
-					<swiper-item>
-						<view class="doctor-brief">
-							<image
-								class="doctor-avatar"
-								src="/static/uploads/doctor-avatar.jpg"
-							/>
-							<view class="doctor-info">
-								<view class="meta">
-									<text class="name">王医生</text>
-									<text class="title"
-										>内分泌科 | 主任医师</text
-									>
-								</view>
-								<view class="meta">
-									<text class="tag">三甲</text>
-									<text class="hospital">积水潭医院</text>
-								</view>
-							</view>
-							<navigator
-								class="doctor-contcat"
-								hover-class="none"
-								url=" "
-							>
-								进入咨询
-							</navigator>
-						</view>
-					</swiper-item>
-					<swiper-item>
-						<view class="doctor-brief">
-							<image
-								class="doctor-avatar"
-								src="/static/uploads/doctor-avatar.jpg"
-							/>
-							<view class="doctor-info">
-								<view class="meta">
-									<text class="name">王医生</text>
-									<text class="title"
-										>内分泌科 | 主任医师</text
-									>
-								</view>
-								<view class="meta">
-									<text class="tag">三甲</text>
-									<text class="hospital">积水潭医院</text>
-								</view>
-							</view>
-							<navigator
-								class="doctor-contcat"
-								hover-class="none"
-								url=" "
-							>
-								进入咨询
-							</navigator>
-						</view>
-					</swiper-item>
-				</swiper>
-			</custom-section>
-			<!-- 药品订单 -->
-			<custom-section show-arrow title="药品订单">
-				<template #right>
-					<navigator hover-class="none" url=" "> 全部订单 </navigator>
-				</template>
-				<view class="drug-order">
-					<navigator hover-class="none" url=" ">
-						<uni-badge
-							:text="0"
-							:offset="[3, 3]"
-							absolute="rightTop"
-						>
-							<image
-								src="/static/images/order-status-1.png"
-								class="status-icon"
-							/>
-						</uni-badge>
-						<text class="status-label">待付款</text>
-					</navigator>
-					<navigator hover-class="none" url=" ">
-						<uni-badge
-							text="2"
-							:offset="[3, 3]"
-							absolute="rightTop"
-						>
-							<image
-								src="/static/images/order-status-2.png"
-								class="status-icon"
-							/>
-						</uni-badge>
-						<text class="status-label">待付款</text>
-					</navigator>
-					<navigator hover-class="none" url=" ">
-						<uni-badge
-							:text="0"
-							:offset="[3, 3]"
-							absolute="rightTop"
-						>
-							<image
-								src="/static/images/order-status-3.png"
-								class="status-icon"
-							/>
-						</uni-badge>
-						<text class="status-label">待付款</text>
-					</navigator>
-					<navigator hover-class="none" url=" ">
-						<uni-badge
-							:text="0"
-							:offset="[3, 3]"
-							absolute="rightTop"
-						>
-							<image
-								src="/static/images/order-status-4.png"
-								class="status-icon"
-							/>
-						</uni-badge>
-						<text class="status-label">待付款</text>
-					</navigator>
-				</view>
-			</custom-section>
-			<!-- 快捷工具 -->
-			<custom-section title="快捷工具">
-				<uni-list :border="false">
-					<uni-list-item
-						:border="false"
-						title="我的问诊"
-						show-arrow
-						show-extra-icon
-						:extra-icon="{
-							customPrefix: 'icon-symbol',
-							type: 'icon-symbol-tool-01',
-						}"
-					/>
-					<uni-list-item
-						:border="false"
-						title="我的处方"
-						show-arrow
-						show-extra-icon
-						:extra-icon="{
-							customPrefix: 'icon-symbol',
-							type: 'icon-symbol-tool-02',
-						}"
-					/>
-					<uni-list-item
-						:border="false"
-						title="家庭档案"
-						show-arrow
-						show-extra-icon
-						:extra-icon="{
-							customPrefix: 'icon-symbol',
-							type: 'icon-symbol-tool-03',
-						}"
-					/>
-					<uni-list-item
-						:border="false"
-						title="地址管理"
-						show-arrow
-						show-extra-icon
-						:extra-icon="{
-							customPrefix: 'icon-symbol',
-							type: 'icon-symbol-tool-04',
-						}"
-					/>
-					<uni-list-item
-						:border="false"
-						title="我的评价"
-						show-arrow
-						show-extra-icon
-						:extra-icon="{
-							customPrefix: 'icon-symbol',
-							type: 'icon-symbol-tool-05',
-						}"
-					/>
-					<uni-list-item
-						:border="false"
-						title="官方客服"
-						show-arrow
-						show-extra-icon
-						:extra-icon="{
-							customPrefix: 'icon-symbol',
-							type: 'icon-symbol-tool-06',
-						}"
-					/>
-					<uni-list-item
-						:border="false"
-						title="设置"
-						show-arrow
-						show-extra-icon
-						:extra-icon="{
-							customPrefix: 'icon-symbol',
-							type: 'icon-symbol-tool-07',
-						}"
-					/>
-				</uni-list>
-			</custom-section>
-			<!-- 退出登录 -->
-			<view class="logout-button">退出登录</view>
-		</view>
-	</scroll-page>
+  <scroll-page background-color="#F6F7F9">
+    <view class="my-page">
+      <!-- 用户资料（头像&昵称） -->
+      <view class="user-profile">
+        <image class="user-avatar" :src="userInfo.avatar"></image>
+        <view class="user-info">
+          <text class="nickname">{{ userInfo.account }}</text>
+          <text class="iconfont icon-edit"></text>
+        </view>
+      </view>
+      <!-- 用户数据 -->
+      <view class="user-data">
+        <navigator hover-class="none" url=" ">
+          <text class="data-number">{{ userInfo.collectionNumber }}</text>
+          <text class="data-label">收藏</text>
+        </navigator>
+        <navigator hover-class="none" url=" ">
+          <text class="data-number">{{ userInfo.likeNumber }}</text>
+          <text class="data-label">关注</text>
+        </navigator>
+        <navigator hover-class="none" url=" ">
+          <text class="data-number">{{ userInfo.score }}</text>
+          <text class="data-label">积分</text>
+        </navigator>
+        <navigator hover-class="none" url=" ">
+          <text class="data-number">{{ userInfo.couponNumber }}</text>
+          <text class="data-label">优惠券</text>
+        </navigator>
+      </view>
+      <!-- 问诊医生 -->
+      <custom-section :custom-style="{ paddingBottom: '20rpx' }" title="问诊中">
+        <swiper
+          class="uni-swiper"
+          indicator-active-color="#2CB5A5"
+          indicator-color="#EAF8F6"
+          indicator-dots
+        >
+          <swiper-item>
+            <view class="doctor-brief">
+              <image
+                class="doctor-avatar"
+                src="/static/uploads/doctor-avatar.jpg"
+              />
+              <view class="doctor-info">
+                <view class="meta">
+                  <text class="name">王医生</text>
+                  <text class="title">内分泌科 | 主任医师</text>
+                </view>
+                <view class="meta">
+                  <text class="tag">三甲</text>
+                  <text class="hospital">积水潭医院</text>
+                </view>
+              </view>
+              <navigator class="doctor-contcat" hover-class="none" url=" ">
+                进入咨询
+              </navigator>
+            </view>
+          </swiper-item>
+          <swiper-item>
+            <view class="doctor-brief">
+              <image
+                class="doctor-avatar"
+                src="/static/uploads/doctor-avatar.jpg"
+              />
+              <view class="doctor-info">
+                <view class="meta">
+                  <text class="name">王医生</text>
+                  <text class="title">内分泌科 | 主任医师</text>
+                </view>
+                <view class="meta">
+                  <text class="tag">三甲</text>
+                  <text class="hospital">积水潭医院</text>
+                </view>
+              </view>
+              <navigator class="doctor-contcat" hover-class="none" url=" ">
+                进入咨询
+              </navigator>
+            </view>
+          </swiper-item>
+        </swiper>
+      </custom-section>
+      <!-- 药品订单 -->
+      <custom-section show-arrow title="药品订单">
+        <template #right>
+          <navigator hover-class="none" url=" "> 全部订单 </navigator>
+        </template>
+        <view class="drug-order">
+          <navigator hover-class="none" url=" ">
+            <uni-badge :text="0" :offset="[3, 3]" absolute="rightTop">
+              <image
+                src="/static/images/order-status-1.png"
+                class="status-icon"
+              />
+            </uni-badge>
+            <text class="status-label">待付款</text>
+          </navigator>
+          <navigator hover-class="none" url=" ">
+            <uni-badge text="2" :offset="[3, 3]" absolute="rightTop">
+              <image
+                src="/static/images/order-status-2.png"
+                class="status-icon"
+              />
+            </uni-badge>
+            <text class="status-label">待付款</text>
+          </navigator>
+          <navigator hover-class="none" url=" ">
+            <uni-badge :text="0" :offset="[3, 3]" absolute="rightTop">
+              <image
+                src="/static/images/order-status-3.png"
+                class="status-icon"
+              />
+            </uni-badge>
+            <text class="status-label">待付款</text>
+          </navigator>
+          <navigator hover-class="none" url=" ">
+            <uni-badge :text="0" :offset="[3, 3]" absolute="rightTop">
+              <image
+                src="/static/images/order-status-4.png"
+                class="status-icon"
+              />
+            </uni-badge>
+            <text class="status-label">待付款</text>
+          </navigator>
+        </view>
+      </custom-section>
+      <!-- 快捷工具 -->
+      <custom-section title="快捷工具">
+        <uni-list :border="false">
+          <uni-list-item
+            :border="false"
+            title="我的问诊"
+            show-arrow
+            show-extra-icon
+            :extra-icon="{
+              customPrefix: 'icon-symbol',
+              type: 'icon-symbol-tool-01',
+            }"
+          />
+          <uni-list-item
+            :border="false"
+            title="我的处方"
+            show-arrow
+            show-extra-icon
+            :extra-icon="{
+              customPrefix: 'icon-symbol',
+              type: 'icon-symbol-tool-02',
+            }"
+          />
+          <uni-list-item
+            :border="false"
+            title="家庭档案"
+            show-arrow
+            show-extra-icon
+            :extra-icon="{
+              customPrefix: 'icon-symbol',
+              type: 'icon-symbol-tool-03',
+            }"
+          />
+          <uni-list-item
+            :border="false"
+            title="地址管理"
+            show-arrow
+            show-extra-icon
+            :extra-icon="{
+              customPrefix: 'icon-symbol',
+              type: 'icon-symbol-tool-04',
+            }"
+          />
+          <uni-list-item
+            :border="false"
+            title="我的评价"
+            show-arrow
+            show-extra-icon
+            :extra-icon="{
+              customPrefix: 'icon-symbol',
+              type: 'icon-symbol-tool-05',
+            }"
+          />
+          <uni-list-item
+            :border="false"
+            title="官方客服"
+            show-arrow
+            show-extra-icon
+            :extra-icon="{
+              customPrefix: 'icon-symbol',
+              type: 'icon-symbol-tool-06',
+            }"
+          />
+          <uni-list-item
+            :border="false"
+            title="设置"
+            show-arrow
+            show-extra-icon
+            :extra-icon="{
+              customPrefix: 'icon-symbol',
+              type: 'icon-symbol-tool-07',
+            }"
+          />
+        </uni-list>
+      </custom-section>
+      <!-- 退出登录 -->
+      <view class="logout-button" @click="onLogoutClick">退出登录</view>
+    </view>
+  </scroll-page>
 </template>
 <script setup>
-	import { ref } from 'vue'
-	import { userInfoApi } from '@/apis/user.js'
-	// 用户信息
-	const userInfo = ref({})
-	// 调用接口获取用户信息
-	const getUserInfo = async () => {
-		try {
-			const { code, data, message } = await userInfoApi()
-			if (code !== 10000) return uni.utils.toast(message)
-			// 渲染用户数据
-			userInfo.value = data
-		} catch (error) {
-			console.error('获取用户信息失败:', error)
+  import { ref } from 'vue'
+  import { userInfoApi } from '@/apis/user.js'
+  import { userInfoStore } from '@/stores/user.js'
+  // 用户信息
+  const userInfo = ref({})
+  // 调用接口获取用户信息
+  const getUserInfo = async () => {
+    try {
+      const { code, data, message } = await userInfoApi()
+      if (code !== 10000) return uni.utils.toast(message)
+      // 渲染用户数据
+      userInfo.value = data
+    } catch (error) {
+      console.error('获取用户信息失败:', error)
+    }
+  }
+  const onLogoutClick = () => {
+     uni.showModal({
+	  title: '退出登录',
+	  content: '您确定要退出登录吗？',
+	  success: (res) => {
+		if (res.confirm) {
+		  // 清除用户信息
+	
+	       const store = userInfoStore()
+		   store.token = ''
+		   store.userInfo = {}
+		   // 清除历史页面重定向路径默认回到首页
+		   store.redirectPath = './pages/index/index'
+		   // 清楚历史重定向路径默认是
+		   store.openType = 'switchTab'
+		   uni.utils.toast('退出登录成功')
+		  // 跳转到登录页
+		  uni.redirectTo({ url: '/pages/login/index' })
 		}
-	}
-	getUserInfo()
+	  }
+	})
+  }
+  getUserInfo()
 </script>
 <style lang="scss">
-	@import './index.scss';
+  @import './index.scss';
 </style>
