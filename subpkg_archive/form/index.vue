45964ca99f7a8bf85a07153ccff759f33d5f6af5
poenao <940837938@qq.com>
2026-04-12 20:45:52 +0800

    创建添加患者分包和模板

---
+++ subpkg_archive/form/index.vue
@@ -0,0 +1,50 @@
+<script setup>
+  import { ref } from 'vue'
+  const isDefault = ref([0])
+</script>
+
+<template>
+  <scroll-page>
+    <view class="archive-page">
+      <uni-forms border label-width="220rpx" ref="form">
+        <uni-forms-item label="患者姓名" name="name">
+          <uni-easyinput
+            placeholder-style="color: #C3C3C5; font-size: 32rpx"
+            :styles="{ color: '#121826' }"
+            :input-border="false"
+            :clearable="false"
+            placeholder="请填写真实姓名"
+          />
+        </uni-forms-item>
+        <uni-forms-item label="患者身份证号" name="name">
+          <uni-easyinput
+            placeholder-style="color: #C3C3C5; font-size: 32rpx"
+            :styles="{ color: '#121826' }"
+            :input-border="false"
+            :clearable="false"
+            placeholder="请填写身份证号"
+          />
+        </uni-forms-item>
+        <uni-forms-item label="患者性别" name="name">
+          <uni-data-checkbox
+            selectedColor="#16C2A3"
+            :localdata="[
+              { text: '男', value: 1 },
+              { text: '女', value: 0 },
+            ]"
+          />
+        </uni-forms-item>
+        <uni-forms-item label="默认就诊人" name="name">
+          <view class="uni-switch">
+            <switch checked color="#20c6b2" style="transform: scale(0.7)" />
+          </view>
+        </uni-forms-item>
+        <button class="uni-button">保存</button>
+      </uni-forms>
+    </view>
+  </scroll-page>
+</template>
+
+<style lang="scss">
+  @import './index.scss';
+</style>
--- utils/request.js
+++ utils/request.js
@@ -1,83 +1,85 @@
 // 导入模块
 import Request from 'luch-request'
-import {
-	userInfoStore
-} from '@/stores/user.js'
+import { userInfoStore } from '@/stores/user.js'
 // tabBar页面路径
 const tabBarList = [
-	'pages/index/index',
-	'pages/wiki/index',
-	'pages/notify/index',
-	'pages/my/index',
+  'pages/index/index',
+  'pages/wiki/index',
+  'pages/notify/index',
+  'pages/my/index',
 ]
 // 实例化网络请求
 const request = new Request({
-	baseURL: 'https://consult-api.itheima.net', // 基础URL
-	timeout: 5000, // 请求超时时间
-	custom: {
-		loading: true, // 是否显示加载提示
-	},
+  baseURL: 'https://consult-api.itheima.net', // 基础URL
+  timeout: 5000, // 请求超时时间
+  custom: {
+    loading: true, // 是否显示加载提示
+  },
 })
 // 接口白名单
 const whiteList = ['/code', '/login', '/login/password']
 // 请求拦截器
 request.interceptors.request.use(
-	(config) => {
-		// 显示加载状态提示
-		const store = userInfoStore()
-		// 全局默认的头信息（方便以后扩展）
-		const defaultHeader = {}
-		// 判断是否存在 token 并且不在接口白单当中
-		if (store.token && !whiteList.includes(config.url)) {
-			defaultHeader.Authorization = 'Bearer ' + store.token
-		}
-		// 合并全局头信息和局部头信息（局部优先级高全局）
-		config.header = {
-			...defaultHeader,
-			...config.header,
-		}
+  (config) => {
+    // 显示加载状态提示
+    const store = userInfoStore()
+    // 全局默认的头信息（方便以后扩展）
+    const defaultHeader = {}
+    // 判断是否存在 token 并且不在接口白单当中
+    if (store.token && !whiteList.includes(config.url)) {
+      defaultHeader.Authorization = 'Bearer ' + store.token
+    }
+    // 合并全局头信息和局部头信息（局部优先级高全局）
+    config.header = {
+      ...defaultHeader,
+      ...config.header,
+    }
 
-		return config
-	},
-	(error) => {
-		// 处理请求错误
-		return Promise.reject(error)
-	}
+    return config
+  },
+  (error) => {
+    // 处理请求错误
+    return Promise.reject(error)
+  }
 )
 // 响应拦截器
 request.interceptors.response.use(
-	(response) => {
-		// 如果响应状态码是401，说明token无效或过期，提示用户重新登录
-		if (response.statusCode === 401) {
-			uni.utils.toast('登录状态已过期，请重新登录!')
-			relogin()
-		}
-		return response.data
-	},
-	(error) => {
-		// 处理响应错误
-		return Promise.reject(error)
-	}
+  (response) => {
+    // 如果响应状态码是401，说明token无效或过期，提示用户重新登录
+    if (response.statusCode === 401) {
+      relogin()
+    }
+    return response.data
+  },
+  (error) => {
+    // 【重点】如果后端直接抛出 401 HTTP 状态码，逻辑会跳到这里！
+    if (error.statusCode === 401 || error.errMsg?.includes('401')) {
+      uni.utils.toast('登录状态已过期，请重新登录')
+      relogin()
+    }
+    // 处理响应错误
+    return Promise.reject(error)
+  }
 )
 
 // 引导用户重新登录
 const relogin = () => {
-	const store = userInfoStore()
-	// 动态读取当前页面的路径
-	const pageStack = getCurrentPages()
-	const currentPage = pageStack[pageStack.length - 1]
-	// 完整的路由包含地址的参数
-	const redirectURL = currentPage.$page.fullPath
-	// 判断当前页面是否在tabBar中然后选择合适的跳转方式
-	const openType = tabBarList.includes(currentPage.route) ?
-		'switchTab' :
-		'navigateTo'
-	//把上一页的页面地址和处理过的跳转方式传递给pinia
-	store.redirectURL = redirectURL
-	store.openType = openType
-	// 跳转到登录页面
-	uni.redirectURL('/pages/login/index')
+  const store = userInfoStore()
+  // 动态读取当前页面的路径
+  const pageStack = getCurrentPages()
+  const currentPage = pageStack[pageStack.length - 1]
+  // 完整的路由包含地址的参数
+  const redirectURL = currentPage.$page.fullPath
+  // 判断当前页面是否在tabBar中然后选择合适的跳转方式
+  const openType = tabBarList.includes(currentPage.route)
+    ? 'switchTab'
+    : 'navigateTo'
+  //把上一页的页面地址和处理过的跳转方式传递给pinia
+  store.redirectURL = redirectURL
+  store.openType = openType
+  // 修改为：
+  uni.redirectTo({
+    url: '/pages/login/index',
+  })
 }
-export {
-	request
-}
+export { request }
