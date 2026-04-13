491d8071ef16751d7b53ff401e71e08cc5f02c84
poenao <940837938@qq.com>
2026-04-13 12:30:41 +0800

    问诊类型-页面布局

---
+++ subpkg_consult/quickly/index.vue
@@ -0,0 +1,35 @@
+<script setup></script>
+
+<template>
+  <scroll-page>
+    <view class="quickly-page">
+      <image class="quickly-banner" src="/static/images/quickly-consult.png" />
+      <view class="quickly-tips">
+        <text class="highlight">20s</text>
+        快速匹配专业医生
+      </view>
+      <uni-list :border="false" class="consult-type">
+        <uni-list-item
+          title="三甲图文问诊"
+          note="三甲主治及以上级别医生"
+          show-arrow
+          thumb="/static/images/consult-type-1.png"
+          thumb-size="lg"
+        />
+      </uni-list>
+      <uni-list :border="false" class="consult-type">
+        <uni-list-item
+          title="普通图文问诊"
+          note="二甲主治及以上级别医生"
+          show-arrow
+          thumb="/static/images/consult-type-2.png"
+          thumb-size="lg"
+        />
+      </uni-list>
+    </view>
+  </scroll-page>
+</template>
+
+<style lang="scss">
+  @import './index.scss';
+</style>
